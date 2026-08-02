de95a223c392cf05bc2a646eb9ecfaf80ab17f8e
angel26dev <angel26lst@gmail.com>
2026-08-01 22:25:35 -0400

    Actualizar main.py
    Actualización del sistema de inventario: registro de productos

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 print("=== Sistema de Inventario para Restaurantes ===")
-
 print("Bienvenido al sistema")
+
+producto = input("Ingrese el nombre del producto: ")
+cantidad = input("Ingrese la cantidad disponible: ")
+
+print("\nProducto registrado:")
+print("Nombre:", producto)
+print("Cantidad:", cantidad)
