7d62fdb4ca40117c16400af9236e5b59cfffb9ca
angel26dev <angel26lst@gmail.com>
2026-08-01 22:27:29 -0400

    Actualizar main.py
    básico de inventario

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 print("=== Sistema de Inventario para Restaurantes ===")
-print("Bienvenido al sistema")
 
-producto = input("Ingrese el nombre del producto: ")
-cantidad = input("Ingrese la cantidad disponible: ")
+inventario = []
 
-print("\nProducto registrado:")
-print("Nombre:", producto)
-print("Cantidad:", cantidad)
+while True:
+    print("\n--- MENÚ PRINCIPAL ---")
+    print("1. Agregar producto")
+    print("2. Ver inventario")
+    print("3. Salir")
+
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == "1":
+        producto = input("Nombre del producto: ")
+        cantidad = input("Cantidad disponible: ")
+
+        inventario.append({
+            "nombre": producto,
+            "cantidad": cantidad
+        })
+
+        print("Producto agregado correctamente.")
+
+    elif opcion == "2":
+        print("\n--- INVENTARIO ACTUAL ---")
+
+        if len(inventario) == 0:
+            print("El inventario está vacío.")
+        else:
+            for producto in inventario:
+                print(
+                    "Producto:",
+                    producto["nombre"],
+                    "| Cantidad:",
+                    producto["cantidad"]
+                )
+
+    elif opcion == "3":
+        print("Cerrando sistema...")
+        break
+
+    else:
+        print("Opción no válida.")
